5a56da16432e031e1ae632c9c67d6b624c2b642c
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2024-10-20 23:17:41 -0300

    first commit

---
+++ giftlist-platform/src/test/java/com/giftlist/app/GiftListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftlist.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
